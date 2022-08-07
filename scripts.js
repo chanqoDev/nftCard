618ded030c4b495ea53725ca045f02046a03f7e0
Christian E <christianelena92@gmail.com>
2022-08-06 23:29:30 -0500

    Dynamically load nftcard data

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
- 
+const nftCard = {
+  title: 'Equilibrium #3420',
+  subTitle: 'Our Equilibrium collection promotes balance and calm.',
+  price: ' 0.041 ETH',
+  time: '3 days left',
+  creatorName: 'Jules Wyvern',
+};
+
+document.querySelector('.title').innerText = `${nftCard.title}`;
+document.querySelector('.subTitle').innerText = `${nftCard.subTitle}`;
+document.querySelector('.price').append(`${nftCard.price}`);
+document.querySelector('.time').append(`${nftCard.time}`);
+document.querySelector('.creatorName').append(`${nftCard.creatorName}`);
